3ee6f1583a8961f760f514bce762067eec4b36dc
ramyasrimanukonda <ramyasrimanukonda@gmail.com>
2025-10-26 19:16:00 +0530

    Update largest.c

--- largest.c
+++ largest.c
@@ -2,8 +2,8 @@
 int main() {
     int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
-    if (a >= b && a >= c) printf("Largest: %d", a);
-    else if (b >= a && b >= c) printf("Largest: %d", b);
+    if (a > b && a > c) printf("Largest: %d", a);
+    else if (b > c) printf("Largest: %d", b);
     else printf("Largest: %d", c);
     return 0;
 }
